491821700c72ad3705f31d48c43b6aece0b597a2
dan850 <dan06982@gmail.com>
2022-12-05 00:59:39 -0600

    update

---
+++ event.js
@@ -0,0 +1,68 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+///////////////////////////////////////////////////////////
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+///////////////////////////
+
+function myFunction() {
+    document.body.style.backgroundColor = "black";
+    
+    const myCollection = document.getElementById(".main");
+    for (let i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "white";
+    }
+    
+  }
+
+
+  function myFunction2() {
+    document.body.style.backgroundColor = "white";
+    
+    const myCollection = document.getElementById(".main");
+    for (let i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "black";
+    }
+    
+  }
+  
